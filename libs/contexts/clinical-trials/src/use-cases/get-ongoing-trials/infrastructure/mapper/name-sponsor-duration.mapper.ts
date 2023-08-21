4cd397a4d92189489e85da5044fe36cc850041c9
Younas <younaslakziz@gmail.com>
2023-08-21 21:58:34 +0200

    feat(clinical-trials): add mapper

---
+++ libs/contexts/clinical-trials/src/use-cases/get-ongoing-trials/infrastructure/mapper/name-sponsor-duration.mapper.ts
@@ -0,0 +1,18 @@
+import { TrialMapper } from '../../../../domain/models/trials/trial-mapper.type';
+import { Trial } from '../../../../domain/models/trials/trial.value-object';
+
+export const nameSponsorDurationTrialMapper: TrialMapper<MappedTrial> = (
+  trial: Trial,
+) => ({
+  name: trial.name,
+  sponsor: trial.sponsor.toString(),
+  start_date: trial.startDate,
+  end_date: trial.endDate,
+});
+
+type MappedTrial = {
+  name: string;
+  start_date: string;
+  end_date: string;
+  sponsor: string;
+};
